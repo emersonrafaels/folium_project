fd095786778ee9b66213f0bf09dfa332f0292d22
Emerson Rafael <40706663+emersonrafaels@users.noreply.github.com>
2023-06-11 03:32:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import base64
 from os import path
 from json import loads
+from pathlib import Path
 
+import branca
 import pandas as pd
 import streamlit as st
 import folium
@@ -8,7 +11,7 @@ import validators
 from streamlit_folium import st_folium
 from streamlit_custom_notification_box import custom_notification_box
 from streamlit_extras.app_logo import add_logo
-from streamlit_extras.dataframe_explorer import dataframe_explorer
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 def add_logo(logo_url: str, height: int = 120):
 
@@ -46,18 +49,32 @@ def add_logo(logo_url: str, height: int = 120):
         unsafe_allow_html=True,
     )
 
-@st.cache_data
+
 def load_data():
 
   # REALIZANDO A LEITURA DOS DADOS
-  df = pd.read_excel("/content/gdrive/MyDrive/FOOTPRINT/footprint_agencias.xlsx")
+  df = pd.read_excel("/content/gdrive/MyDrive/FOOTPRINT/BASE_COM_CEP.xlsx")
 
   return df
 
-@st.cache_data
+
 def download_data(df):
 
-  return df.to_csv(index=None).encode('latin1')
+  # INICIALIZANDO O VALIDATOR
+  validator = False
+
+  try:
+    df.to_excel("FOOTPRINT.xlsx", index=None)
+
+    validator = True
+
+    if validator:
+
+      pass
+
+      # st.success('Dados salvos com sucesso', icon="✅")
+  except Exception as ex:
+    print(ex)
 
 def download_map(mapobj):
 
@@ -77,7 +94,69 @@ def download_map(mapobj):
   except Exception as ex:
     print(ex)
 
-def load_map():
+def convert_df_html(row_df, 
+                    col_header=None,
+                    left_col_color="#140083", 
+                    right_col_color="#140083", 
+                    left_text_color="#FFFFFF", 
+                    right_text_color="#FFFFFF"):
+
+  # INICIANDO A VARIÁVEL DE RETORNO
+  html = ""
+
+  # INICIANDO A VARIÁVEL AUXILIAR QUE ARMAZENARÁ AS TABLES
+  html_table = ""
+
+  html_header = """<!DOCTYPE html>
+      <html>
+      <head>
+        <h4 style="margin-bottom:10"; width="200px">{}</h4>""".format(row_df.get(col_header)) + """
+        <style>
+      table {
+        border:1px solid #b3adad;
+        border-collapse:collapse;
+        padding:5px;
+      }
+      table th {
+        border:1px solid #b3adad;
+        padding:5px;
+        background: #f0f0f0;
+        color: #313030;
+      }
+      table td {
+        border:1px solid #b3adad;
+        text-align:center;
+        padding:5px;
+        background: #ffffff;
+        color: #313030;
+      }
+    </style>
+      </head>
+          <table style="height: 126px; width: 350px;">
+      <tbody>
+    """
+
+  # VERIFICANDO SE O ARGUMENTO É UM DICT
+  if isinstance(row_df, (dict, pd.Series)):
+
+    # PERCORRENDO O DICT
+    for key, value in row_df.items():
+
+      html_table += """
+        <tr>
+        <td style="background-color: """+ left_col_color + ";font-weight: bold"""";"><span style="color: text_left_color_to_replace;">key_to_replace</span></td>
+        <td style="width: 150px;background-color: """+ right_col_color +""";"><span style="color: right_left_color_to_replace;">value_to_replace</span></td>
+        </tr>
+      """
+      html_table = html_table.replace("key_to_replace", str(key)).replace("value_to_replace", str(value))
+      html_table = html_table.replace("text_left_color_to_replace", str(left_text_color)).replace("right_left_color_to_replace", str(right_text_color))
+
+    # UNINDO OS HTML
+    html = "{}{}".format(html_header, html_table)
+
+  return html
+
+def load_map(data=None, circle_radius=0):
 
   def add_layers_control(mapobj, validator_add_layer=True):
 
@@ -95,44 +174,87 @@ def load_map():
 
   def add_markers(mapobj, data=None, circle_radius=0):
 
-    folium.Marker(
-      location=[-23.536791847273886, -46.7784728049161],
-      popup="ITAU OSASCO",
-      icon=folium.Icon(color='red', icon="glyphicon glyphicon-briefcase"),
-  ).add_to(mapobj)
+    # VERIFICANDO SE HÁ UM DATAFRAME ENVIADO COMO ARGUMENTO
+    if data is not None:
+      # PERCORRENDO O DATAFRAME
+      for idx, row in data.iterrows():
 
-    folium.Marker(
-        location=[-23.56863373566806, -46.648261720258745],
-        popup="AV PAULISTA I",
-        icon=folium.Icon(color="green", icon="glyphicon glyphicon-briefcase"),
-    ).add_to(mapobj)
+        # OBTENDO O STATUS
+        status = row.get("STATUS")
 
-    folium.Marker(
-        location=[-23.5644555638821, -46.653611733750736],
-        popup="AV PAULISTA II",
-        icon=folium.Icon(color="orange", icon="glyphicon glyphicon-briefcase"),
-    ).add_to(mapobj)
+        # OBTENDO LATITUDE E LONGITUDE
+        lat = row.get("LATITUDE")
+        long = row.get("LONGITUDE")
 
-    if circle_radius > 0:
+        # OBTENDO O HTML DO ICON
+        html = convert_df_html(row_df=row, col_header="ENDEREÇO", 
+                              left_col_color="#140083", 
+                              right_col_color="#140083", 
+                              left_text_color="#FF7200", 
+                              right_text_color="#FFFFFF")
+        iframe = branca.element.IFrame(html=html, width=510, height=280)
+        popup = folium.Popup(folium.Html(html, script=True), max_width=500)
 
-      # MULTIPLICA O RAIO POR 1000, PARA REALIZAR A CONVERSÃO DE M PARA KM
-      folium.Circle(radius=circle_radius*1000, 
-                    location=[[-23.536791847273886, -46.7784728049161]]).add_to(mapobj)
+        if str(status).upper() == "VERMELHA":
+          current_icon = folium.Icon(color='red', icon="glyphicon glyphicon-briefcase")
+        elif str(status).upper() == "AMARELA":
+          current_icon = folium.Icon(color='orange', icon="glyphicon glyphicon-briefcase")
+        else:
+          current_icon = folium.Icon(color="green", icon="glyphicon glyphicon-briefcase")
+
+        folium.Marker(
+          location=[lat, long],
+          popup=popup, 
+          icon=current_icon, 
+          lazy=True
+      ).add_to(mapobj)
 
     return mapobj
 
-  footprint_map = folium.Map(location=[-23.561118452973457, -46.656451389570975], 
-                            zoom_start=12, 
-                            tiles="Stamen Terrain")
+  footprint_map = folium.Map(location=[-15.768857589354258, -47.905384728712384], 
+                             zoom_start=4, 
+                             tiles="openstreetmap")
 
   # ADICIONANDO LAYERS
   footprint_map = add_layers_control(mapobj=footprint_map, 
-                                    validator_add_layer=True)
+                                     validator_add_layer=True)
 
   # ADICIONANDO MAKERS
-  footprint = add_markers(mapobj=footprint_map, data=None)
+  footprint_map = add_markers(mapobj=footprint_map, 
+                              data=data, 
+                              circle_radius=circle_radius)
 
   return footprint_map
+
+def convert_dataframe_to_aggrid(data, 
+                                validator_all_rows_selected=True):
+
+  gb = GridOptionsBuilder.from_dataframe(data)
+  gb.configure_pagination(paginationAutoPageSize=True, 
+                          paginationPageSize=5) #Add pagination
+  gb.configure_side_bar(filters_panel=True, columns_panel=True, defaultToolPanel="") #Add a sidebar
+  gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+  
+  # VALIDANDO SE É DESEJADO QUE TODAS AS LINHAS INICIEM SELECIONADAS
+  if validator_all_rows_selected:
+    gb.configure_selection('multiple', pre_selected_rows=list(range(len(data))))
+
+  gridOptions = gb.build()
+
+  grid_response = AgGrid(
+      data,
+      gridOptions=gridOptions,
+      data_return_mode='AS_INPUT', 
+      update_mode='MODEL_CHANGED', 
+      fit_columns_on_grid_load=False,
+      theme='streamlit',
+      enable_enterprise_modules=True,
+      height=350, 
+      width='100%',
+      reload_data=False
+  )
+
+  return grid_response
 
 def main():
 
@@ -140,11 +262,6 @@ def main():
   st.set_page_config(page_title="FOOTPRINT - GESTÃO DO PARQUE DE AGÊNCIAS", 
                      page_icon=":world-map:", 
                      layout="wide", 
-                     menu_items={
-                                'Get Help': None,
-                                'Report a bug': None,
-                                'About': "# APP Desenvolvido pela SQUAD Gestão do Parque de Agências"
-                            }
                      )
 
   # ADICIONANDO TITULO DA PÁGINA
@@ -152,6 +269,9 @@ def main():
 
   # ADICIONANDO LOGO
   add_logo(logo_url="http://placekitten.com/120/120")
+
+  # INICIANDO A VARIÁVEL QUE CONTROLA O RAIO DE SOMBREAMENTO
+  raio_sombreamento = 0
 
   with st.sidebar:
 
@@ -181,13 +301,16 @@ def main():
     df_footprint = load_data()
 
     # INCLUINDO O DATAFRAME EM TELA
-    st.markdown("### Dados do footprint")
-    filtered_df = dataframe_explorer(df_footprint, case=False)
-    st.dataframe(filtered_df, use_container_width=True)
+    st.markdown("### Autosserviço - Dados do footprint")
+    dataframe_aggrid = convert_dataframe_to_aggrid(data=df_footprint)
 
-    # CRIANDO MAPA
-    mapobj = load_map()
+    # OBTENDO O DATAFRAME DAS LINHAS SELECIONADAS
+    selected_df = pd.DataFrame(dataframe_aggrid["selected_rows"])
 
+    if not selected_df.empty:
+
+      selected_df = selected_df[df_footprint.columns]
+    
     # INCLUINDO NO APP
     st.markdown("### Parque de agências")
     st_col1, st_col2 = st.columns(2)
@@ -195,33 +318,70 @@ def main():
       result_view_sombreamento = st.checkbox("Visualizar sombreamento", 
                                               value=False, 
                                               key=None, 
-                                              help=None, 
+                                              help="Habilita o controle de sombreamento sobre o mapa", 
                                               on_change=None, 
                                               disabled=False, 
                                               label_visibility="visible")
     if result_view_sombreamento:
       with st_col2: 
-        raio_sombreamento = st.slider('Raio desejado (em km):', 0, 1000, 5)
+        raio_sombreamento = st.slider('Raio desejado (em km):',
+                                      min_value=0, 
+                                      max_value=1000, 
+                                      step=1, 
+                                      help="O raio é aplicado sobre o mapa", )
+    
+    # CRIANDO MAPA
+    mapobj = load_map(data=selected_df, 
+                      circle_radius=raio_sombreamento)
 
+    # INCLUINDO O MAPA NO APP
     st_data = st_folium(mapobj, width=1000, height=500)
 
     st_col1, st_col2 = st.columns(2)
 
     with st_col1:
 
-      st.download_button(
-      label="Download dados (csv)",
-      data=download_data(df_footprint),
-      file_name='FOOTPRINT_DADOS.csv',
-      mime='text/csv',
+      st.button(
+      label="Download dados (excel)",
+      on_click=download_data, 
+      args=(df_footprint,),
       )
 
     with st_col2:
 
       st.button(
           label="Download mapa",
-          on_click=download_map(mapobj)
+          on_click=download_map,
+          args=(mapobj,)
       )
+
+  elif selected_estudo_desejado == "Encerramento":
+
+    # CARREGANDO DATAFRAME
+    df_footprint = load_data()
+
+    # INCLUINDO O DATAFRAME EM TELA
+    st.markdown("### Análise de encerramento")
+    st.markdown("#### Selecione as agências que você deseja analisar")
+    dataframe_aggrid_encerramento = convert_dataframe_to_aggrid(data=df_footprint, 
+                                                   validator_all_rows_selected=False)
+
+    # OBTENDO O DATAFRAME DAS LINHAS SELECIONADAS
+    selected_df_encerramento = pd.DataFrame(dataframe_aggrid_encerramento["selected_rows"])
+
+    if not selected_df_encerramento.empty:
+
+      selected_df_encerramento = selected_df_encerramento[df_footprint.columns]
+
+      st.markdown("### Agências selecionadas")
+      st.dataframe(selected_df_encerramento)
+
+      st.markdown("### Faça upload do arquivo de simulação de encerramento")
+      uploaded_file = st.file_uploader("Escolha o arquivo", 
+                                       type=["csv", "xlsx"], 
+                                       help="O arquivo deve conter as agências selecionadas, caso contrário, não será possível realizar a comparação")
+      if uploaded_file is not None:
+        st.sucess("UPLOAD REALIZADO COM SUCESSO")
 
   else:
       st.markdown("### Feature em desenvolvimento")
